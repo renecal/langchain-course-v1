98f950888160abbd011d4f5062e1e834dab6715a
Rene Calfin <rcalfino@emeal.nttdata.com>
2025-08-22 18:20:48 -0400

    langchain init config

---
+++ main.py
@@ -0,0 +1,12 @@
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+
+def main():
+    print("Hello from langchain-course-1!")
+    print(os.environ.get("GOOGLE_API_KEY"))
+
+
+if __name__ == "__main__":
+    main()
