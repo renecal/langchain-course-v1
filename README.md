52127abc00ce20f8c71cb4a999fe944d30844b97
Rene Calfin <rcalfino@emeal.nttdata.com>
2025-08-27 22:31:51 -0400

    example wit pydantic and ouput validated and format json, add prompt.py with instructions format for agente React

--- README.md
+++ README.md
@@ -2,9 +2,9 @@
 In this branch we will create an AI Job Search Agent using LangChain.
 Using the LangChain framework, we will build a job search agent that can help users find relevant job listings based on their skills and preferences.
 Tavily from search result in internet
-## doc langchain
+## Doc branch
 
-## tools
+### Tools
 
 - [LangChain](https://langchain.com/)
 - [LangSmith Hub](https://smith.langchain.com/hub?organizationId=5c031c7d-225f-41cf-9def-21161772e1fa)
@@ -12,3 +12,9 @@ Tavily from search result in internet
 - [LangChain Documentation](https://langchain.readthedocs.io/en/latest/)
 - [Tools](https://python.langchain.com/docs/integrations/tools/)
 - [Tavily Search](https://python.langchain.com/docs/integrations/tools/tavily_search/)
+
+### Pydantic
+Pydantic is a Python library that provides data validation and settings management using Python's type hints, ensuring data integrity and enabling robust, reliable code. It creates models that enforce data types and structures at runtime, making it easier to work with dynamic data, catch errors early, and serialize/deserialize data into formats like JSON. 
+
+- [Output Parser](https://python.langchain.com/docs/concepts/output_parsers/)
+- [LangChain PydanticOutputParser](https://python.langchain.com/api_reference/core/output_parsers/langchain_core.output_parsers.pydantic.PydanticOutputParser.html)
--- main.py
+++ main.py
@@ -2,8 +2,14 @@ from dotenv import load_dotenv
 from langchain import hub
 from langchain.agents import AgentExecutor
 from langchain.agents.react.agent import create_react_agent
+from langchain_core.output_parsers.pydantic import PydanticOutputParser
+from langchain_core.prompts import PromptTemplate
+from langchain_core.runnables import RunnableLambda
 from langchain_google_genai import ChatGoogleGenerativeAI
 from langchain_tavily import TavilySearch
+
+from prompt import REACT_PROMPT_WITH_FORMAT_INSTRUCTIONS
+from schemas import AgentResponse
 
 load_dotenv()
 
@@ -16,8 +22,27 @@ llm = ChatGoogleGenerativeAI(model="gemini-2.5-flash")
 # Obtiene el prompt reactivo desde el hub de LangChain
 react_prompt = hub.pull("hwchase17/react")
 
+# Crea el analizador de salida
+output_parser = PydanticOutputParser(pydantic_object=AgentResponse)
+
+# Crea el prompt reactivo con instrucciones de formato
+react_prompt_with_format_instructions = PromptTemplate(
+    template=REACT_PROMPT_WITH_FORMAT_INSTRUCTIONS, 
+    input_variables=[
+        "input",
+        "agent_scratchpad",
+        "tool_names"
+    ],
+).partial(
+    format_instructions=output_parser.get_format_instructions(),
+)
+
 # Crea el agente React con el modelo, tools y prompt
-agent = create_react_agent(llm=llm, tools=tools, prompt=react_prompt)
+agent = create_react_agent(
+    llm=llm,
+    tools=tools,
+    prompt=react_prompt_with_format_instructions    
+)
 
 # Ejecuta el agente con las tools y activa el modo detallado
 agent_executor = AgentExecutor(
